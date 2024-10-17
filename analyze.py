18314d20d2856e617adc289dd9f1fe2b84d4cc47
doheon114 <doheon114@naver.com>
2024-10-17 14:12:15 +0000

    hi

--- analyze.py
+++ analyze.py
@@ -6,27 +6,31 @@ matplotlib.use('Agg')
 import numpy as np
 from statannotations.Annotator import Annotator
 sns.set_theme(style="ticks", palette="pastel")
+from scipy.stats import pearsonr, spearmanr
 
-# df = pd.read_excel("histogram.xlsx", index_col=0)
-# #df = df.loc[df["layer"] >= 12]
-# g = sns.catplot(
-#     data=df, kind="bar",
-#     x="layer", y=0, hue="prognosis", palette="dark", alpha=.6, height=6
-# )
-# g.despine(left=True)
-# g.set_axis_labels("", "Volume(%)")
-# g.legend.set_title("")
-# plt.savefig("histogram.png")
 
-x = "1, good 2, poor"
-for y in ["volume(%)", "z score"]:
-    df = pd.read_excel("results/table.xlsx", index_col=0)
+x = "z score"
+dfs = pd.read_excel(f"results/table.xlsx", sheet_name=None)
+for key, df in dfs.items():
     print(df)
-    ax = sns.boxplot(x=x, y=y, data=df)
-    annot = Annotator(ax, [(1, 2)], data=df, x=x, y=y)
-    annot.configure(test='Mann-Whitney', text_format='star', loc='outside', verbose=2)
-    annot.apply_test()
-    ax, test_results = annot.annotate()
-    plt.ylabel(y)
-    plt.savefig(f"results/figure({y}).png")
+    df.columns = ["Prognosis", "Qa", "volume(%)", "z score", 'vmin']
+    df = df.replace({1: "good", 2: "poor"})
+    # for y in ["Qa", "volume(%)", "z score"]:
+    #     ax = sns.boxplot(x=x, y=y, data=df)
+    #     annot = Annotator(ax, [("good", "poor")], data=df, x=x, y=y)
+    #     annot.configure(test='Mann-Whitney-ls', text_format='star', loc='outside', verbose=2)
+    #     annot.apply_test()
+    #     ax, test_results = annot.annotate()
+    #     plt.ylabel(y)
+    #     plt.savefig(f"results/figure({y}).png")
+    #     plt.clf()
+
+    lm = sns.lmplot(data=df, x=x, y="Qa", hue="Prognosis", fit_reg=False)
+    sns.regplot(data=df, x=x, y="Qa", scatter=False, ax=lm.axes[0, 0], robust=True)
+    lm.set(xlabel=x, ylabel="log₁₀Qa", title='')
+    # lm.axes[0, 0].set_xlim(left=0.02, right=0.11)
+
+    r, p = pearsonr(df["Qa"], df[x], alternative="greater")
+    lm.axes[0, 0].text(0.01, -1.5, "R² = {:.3f}, p = {:.4f}".format(r, p))
+    plt.savefig(f"results/correlation_{key}.png")
     plt.clf()
--- main.py
+++ main.py
@@ -2,113 +2,114 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 from multiprocessing import cpu_count, Pool, Manager
-from fsl.wrappers import flirt
-import nibabel as nib
-import subprocess
-from scipy.stats import norm
-from scipy.ndimage import binary_erosion
-from statsmodels.stats.multitest import fdrcorrection
+from scipy.stats import pearsonr, spearmanr
 import pandas as pd
-from nilearn.image import new_img_like, smooth_img
-from nilearn.plotting import plot_stat_map
-import cv2
-import cc3d
+from bayes_opt import BayesianOptimization
+from bayes_opt.logger import JSONLogger
+from bayes_opt.event import Events
+from utils import quantify_vol
+from sklearn.model_selection import KFold
 
-def FLIRT(subject):
-    if not os.path.exists(f"data/e_aligned/{subject}0001.dcm_dicom_FLAIR_AX_CE_SENSE.nii"):
-        flirt(
-            f"data/e/{subject}0001.dcm_dicom_FLAIR_AX_CE_SENSE.nii",
-            f"data/ne/{subject}0001.dcm_dicom_T2_FLAIR_AX_SENSE.nii",
-            out=f"data/e_aligned/{subject}0001.dcm_dicom_FLAIR_AX_CE_SENSE.nii",
-            twod=True,
-        )
+datetime_boundary = "2021-01-01"
+init_points=1
+n_iter=1
+seed=0
 
+def obj_func(vmin, kernel):
 
-def quantify_vol(subject, prognosis):
-    # mask = nib.load(os.path.join("data/ne", subject, "_seg.nii")).get_fdata()
-    # gm = mask > 2.5
-    mask = nib.load(
-        os.path.join("data/spm", f"c1{subject}0001.dcm_dicom_T2_FLAIR_AX_SENSE.nii")
-    ).get_fdata()
-    gm = mask > 0
-
-    ne_ori = nib.load(f"data/ne/{subject}0001.dcm_dicom_T2_FLAIR_AX_SENSE.nii")
-    e_ori = nib.load(f"data/e_aligned/{subject}0001.dcm_dicom_FLAIR_AX_CE_SENSE.nii")
-
-    # ne = smooth_img(ne_ori, fwhm=(1,1,0)).get_fdata()
-    # e = smooth_img(e_ori, fwhm=(1,1,0)).get_fdata()
-    ne = ne_ori.get_fdata()
-    e = e_ori.get_fdata()
-
-    z_map = e - ne / ne[gm].mean() * e[gm].mean()
-    z_map /= z_map[gm].std()
-    p_values = norm.sf(z_map[gm])
-    contrast_mask, _ = fdrcorrection(p_values, alpha=0.05)
-
-    threshold = z_map[gm][contrast_mask].min()
-    z_map[gm == False] = 0
-    contrast_mask = z_map >= threshold
-    
-    labels_out = cc3d.connected_components(contrast_mask)
-    stats = cc3d.statistics(labels_out)
-    sizes = stats["voxel_counts"][1:]
-    cc3d.dust(contrast_mask, threshold=sizes.mean() + norm.ppf(0.95) * sizes.std(), connectivity=26, in_place=True)
-    z_map[contrast_mask == False] = 0
-
-    z_map_img = new_img_like(e_ori, z_map)
-    mean_pv = contrast_mask.sum() / gm.sum() * 100
-    avg_z = z_map.sum() / gm.sum()
-    
-    plot_stat_map(
-        z_map_img,
-        e_ori,
-        output_file=f"results/e/{prognosis}/{subject}.png",
-        threshold=threshold,
-        title=f"{subject}/{prognosis}/e/{mean_pv:.2f}%/z={avg_z:.4f}",
-        radiological=True,
-        display_mode="z",
-    )
-    plot_stat_map(
-        z_map_img,
-        ne_ori,
-        output_file=f"results/ne/{prognosis}/{subject}.png",
-        threshold=threshold,
-        title=f"{subject}/{prognosis}/ne/{mean_pv:.2f}%/z={avg_z:.4f}",
-        radiological=True,
-        display_mode="z",
-    )
-    
-    return mean_pv, avg_z
-
-
-if __name__ == "__main__":
-    plt.rcParams["figure.dpi"] = 1200
-    outcome = pd.read_excel("data/outcome.xlsx", header=1, index_col=0, usecols="A,C")
+    dv = float("inf")
+    size_max = 0
     processes = cpu_count()
+    
+    subjects, prognosis, qa = get_meta_info(dataset)
+    subjects = np.array_split(subjects, processes)
+    subjects = [list(s) for s in subjects]
+    prognosis = np.array_split(prognosis, processes)
+    prognosis = [list(p) for p in prognosis]
+    qa = np.array_split(np.log10(qa), processes)
+    # qa = np.array_split(qa, processes)
+    qa = [list(q) for q in qa]
 
     manager = Manager()
-    good, poor = manager.list(), manager.list()
-    
-    subjects = np.array_split(outcome.index.values, processes)
-    subjects = [list(s) for s in subjects]
-    prognosis = np.array_split(outcome["1, good 2, poor"].values, processes)
-    prognosis = [list(p) for p in prognosis]
+    good_list = manager.list()
+    poor_list = manager.list()
 
-    def do(sub, prog):
-        for s, p in zip(sub, prog):
-            FLIRT(s)
-            percentage_vol, avg_z_score = quantify_vol(s, "good" if p == 1 else "poor")
-            print(s, p, percentage_vol, avg_z_score)
-            # if(percentage_vol.shape[0] == 24):
-            #     if outcome.loc[int(subject), "1, good 2, poor"] == 1:
-            #         good.append(percentage_vol)
-            #     else:
-            #         poor.append(percentage_vol)
+    global do
+    def do(sub, prog, qa):
+        plot = False if phase == "train" else True
+        for s, p, q in zip(sub, prog, qa):
+            if p == 1:
+                percentage_vol, avg_z_score = quantify_vol(s, "good", vmin, vmin + dv, kernel, plot)
+                good_list.append([p, q, percentage_vol, avg_z_score])
+            else:
+                percentage_vol, avg_z_score = quantify_vol(s, "poor", vmin, vmin + dv, kernel, plot)
+                poor_list.append([p, q, percentage_vol, avg_z_score])
 
     with Pool(processes=processes) as pool:
-        pool.starmap(do, zip(subjects, prognosis))
+        pool.starmap(do, zip(subjects, prognosis, qa))
 
-    # histogram = pd.DataFrame(np.concatenate((good + poor)))
-    # histogram["layer"] = np.tile(np.arange(24), len(good + poor))
-    # histogram["prognosis"] = ["good"] * (len(good) * 24) + ["poor"] * (len(poor) * 24)
-    # histogram.to_excel("histogram.xlsx")
+    good = np.array(good_list)[:, [1, 3]]  
+    poor = np.array(poor_list)[:, [1, 3]]
+
+    # good[:, 1] = np.log10(good[:, 1])
+    # poor[:, 1] = np.log10(poor[:, 1])
+
+    if phase == "train":
+        # return pearsonr(np.concatenate((good[:, 0], poor[:, 0])), np.concatenate((good[:, 1], poor[:, 1])))[0]
+        return spearmanr(np.concatenate((good[:, 0], poor[:, 0])), np.concatenate((good[:, 1], poor[:, 1])))[0]
+    else:
+        return list(good_list) + list(poor_list)
+
+def get_meta_info(dset):
+    qa1 = dset[~dset['Qa1'].isna()]
+    id1 = qa1["ID"].astype(int).astype(str).str.zfill(8).values
+    out1 = qa1["outcome"].astype(int).values
+    qa1 = qa1["Qa1"].values
+
+    qa2 = dset[~dset['Qa2'].isna()]
+    id2 = qa2["ID"].astype(int).astype(str).values
+    out2 = qa2["outcome"].astype(int).values
+    qa2 = qa2["Qa1"].values
+
+    qa = np.concatenate((qa1, qa2))
+    out = np.concatenate((out1, out2))
+    id = np.concatenate((id1, id2))
+
+    valid = []
+    for subject in id:
+        subject = f"c1{subject}_T2_FLAIR_AX_SENSE.nii" if subject[0] == "0" else f"c1{subject}0001.dcm_dicom_T2_FLAIR_AX_SENSE.nii"
+        valid.append(True if subject in os.listdir("data/spm_seg/gm") else False)
+
+    return id[valid], out[valid], qa[valid]
+
+if __name__ == "__main__":
+
+    df = pd.read_excel("data/outcome.xlsx", header=0, usecols="A,C,G,H,I").iloc[:75]
+    two_samples = df[(~df['Qa1'].isna()) & (~df['Qa2'].isna())]
+    one_sample = df[(~df['Qa1'].isna()) ^ (~df['Qa2'].isna())]
+    df[(~df['Qa1'].isna()) | (~df['Qa2'].isna())].to_excel("outcome.xlsx")
+    kf = KFold(3, shuffle=True, random_state=seed)
+    plt.rcParams["figure.dpi"] = 1200
+    df_list = []
+    for two_samples_index, one_sample_index in zip(kf.split(two_samples), kf.split(one_sample)):
+        phase = 'train'
+        pbounds = {"vmin": (0.0, 0.5), "kernel": (0, 0.999)}
+        dataset = pd.concat([two_samples.iloc[two_samples_index[0]], one_sample.iloc[one_sample_index[0]]])
+
+        optimizer = BayesianOptimization(f=obj_func, pbounds=pbounds, random_state=seed, allow_duplicate_points=True)
+        logger = JSONLogger(path="./logs.log")
+        optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
+        optimizer.maximize(init_points=init_points, n_iter=n_iter)
+        print(optimizer.max)
+        
+        phase = 'test'
+        dataset = pd.concat([two_samples.iloc[two_samples_index[1]], one_sample.iloc[one_sample_index[1]]])
+
+        params = optimizer.max['params']
+        outcome = np.array(obj_func(params['vmin'], params['kernel']))
+        outcome = np.hstack((outcome, np.tile(np.array([params['vmin']]), (outcome.shape[0], 1))))
+        df_list.append(pd.DataFrame(outcome, columns=["1, good 2, poor", "Qa", "volume(%)", "z score", 'vmin']))
+
+    with pd.ExcelWriter("results/table.xlsx") as writer:
+        for i, table in enumerate(df_list):
+            table.to_excel(writer, sheet_name=f"cv{i}", index=False)
